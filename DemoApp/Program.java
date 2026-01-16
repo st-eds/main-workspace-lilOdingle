e2ea3b89fd176ee762089d3c25306dc3d24b0f2b
lilOdingle <kananomar@sehs.net>
2026-01-16 16:48:41 -0500

    Update Program.java

--- DemoApp/Program.java
+++ DemoApp/Program.java
@@ -1,5 +1,11 @@
 public class Program {
+    public void doubleValue(int x) {
+x = x * 2;
+}
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int num = 2;
+        doubleValue(num);
+        System.out.println("num");
     }
 }
